cc74a8d46e272c7445b877b93c608fac79298436
ikun <ikun@local.com>
2025-06-20 14:30:25 +0800

    commit

---
+++ script/shell/entrance.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+# 定义脚本信息数组
+scripts=(
+    "配置linux远程桌面环境:install_xfce_rdp.sh"
+    "Linux安装Android Studio:install_android_studio.sh"
+)
+
+if [ -z "$1" ]; then
+    echo "请输入序号执行脚本"
+    for i in "${!scripts[@]}"; do
+        echo "$i--- ${scripts[$i]%%:*}"
+    done
+    read index
+else
+    index="$1"
+fi
+
+
+prefix_url="https://raw.githubusercontent.com/xcorga/public/refs/heads/main/script/shell/"
+
+if [ "$index" -ge 0 ] && [ "$index" -lt "${#scripts[@]}" ]; then
+    # 获取脚本信息
+    script_info="${scripts[$index]}"
+    
+    # 提取脚本名称和文件名
+    script_name="${script_info%%:*}"
+    script_file="${script_info#*:}"
+
+    echo "执行脚本: $script_name"
+    sudo /bin/bash -c "$(curl -fsSL "${prefix_url}${script_file}")"
+else 
+    echo "没找到'$index'对应的操作"
+fi
